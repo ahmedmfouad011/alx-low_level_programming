4099e384d5537e5cedaf1cf45d2af1e34ba5c607
ahmedmfouad <ahmedmfouad@gmail.com>
2023-07-17 09:11:23 -0700

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,9 +2,10 @@
 
 /**
  * main - Entry point
+ *
  * Description: write a program that prints all possible
- * differnet combinations of two digits
- * Return: 0
+ *		differnet combinations of two digits
+ * Return: Always 0 (success)
 */
 
 int main(void)
