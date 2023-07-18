142ab3acd69041d717d963c8e73a1364b0bc6a5f
ahmedmfouad <ahmedmfouad@gmail.com>
2023-07-18 08:08:07 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 			prod = num * mult;
 
 			/*
-			 * put space if product is single number
+			 * put space if product is a single number
 			 * place the first digit if its two numbers
 			*/
 			if (prod <= 9)
